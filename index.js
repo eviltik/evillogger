881c773f855b5936b5750c571c3ecba87e837abf
eviltik <eviltik@gmail.com>
2018-03-03 11:41:42 +0100

    remove winston dep

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
-const winston = require('winston');
 const util = require('util');
 const fs = require('fs');
 const sprintf = require('sprintf').sprintf;
 const cluster = require('cluster');
+const colors = require('colors/safe');
 
 class EvilLogger {
 
     constructor(options) {
 
-        if (typeof options === 'string') {
+        if (typeof options === 'string' || !options) {
             options = {
                 ns:options
             }
         }
 
-        this.ns = options.ns || 'master';
+        if (cluster.cid && !options.ns) {
+            this.ns = cluster.cid;
+        } else {
+            this.ns = options.ns || 'root';
+        }
+
         this.spaces = options.spaces || 20;
         this.colorize = true;
         if (typeof options.colorize === 'boolean') {
@@ -28,86 +33,94 @@ class EvilLogger {
 
         let argz = require('minimist')(process.argv.slice(2));
 
-        let opts = {
-            json: false,
-            colorize: this.colorize,
-            timestamp: () => {
-                let d = new Date();
-                let h = (d.getHours() < 10 ? "0" : "") + d.getHours();
-                let m = (d.getMinutes() < 10 ? "0" : "") + d.getMinutes();
-                let s = (d.getSeconds() < 10 ? "0" : "") + d.getSeconds();
-                let mm = d.getMilliseconds();
-
-                if (mm < 10) mm = '0' + mm;
-                if (mm < 100) mm = '0' + mm;
-
-                let date = h + ':' + m + ':' + s + '.' + mm;
-                let ms = ' | ' + sprintf('%'+this.spaces+'s', (this.ns));
-                if (cluster.forkNumber) {
-                    ms+='#'+cluster.forkNumber;
-                } else {
-                    ms+='#0';
-                }
-                return date + ms;
+        if (argz.debug || process.env.DEBUG) {
+            // do not use () => {} syntax here, arguments will be empty
+            this.debug = function() {
+                this._handleLastMessage(arguments, 'debug');
             }
-        };
+        } else {
+            this.debug = () => {};
+        }
 
-        if (argz.debug || process.env.DEBUG) opts.level = 'debug';
+        if (argz.nocolor) {
+            this.colorize = false;
+        }
 
-        let transportConsole = new (winston.transports.Console)(opts);
-        let transports = [transportConsole];
-        this.winstonLogger = new (winston.Logger)({transports: transports});
+        this.colors = {
+            info:'white',
+            error:'red',
+            warn:'yellow',
+            debug:'gray'
+        }
 
+    }
+
+    _prefix(level, msg) {
+        let d = new Date();
+
+        let mm = d.getMilliseconds();
+        if (mm < 10) mm = '0' + mm;
+        if (mm < 100) mm = '0' + mm;
+
+        let str = '';
+        str =(d.getHours() < 10 ? "0" : "") + d.getHours()+':';
+        str+=(d.getMinutes() < 10 ? "0" : "") + d.getMinutes()+':';
+        str+=(d.getSeconds() < 10 ? "0" : "") + d.getSeconds()+'.';
+        str+=mm;
+
+        str+=sprintf(' %2s | %-'+this.spaces+'s | %s: ', cluster.forkNumber||0, this.ns, level);
+
+        str+=msg;
+
+        if (this.colorize) str = colors[this.colors[level]](str);
+
+        return str;
     }
 
     info() {
         arguments[0] = ' ' + arguments[0];
-        this.handleLastMessage(arguments, this.winstonLogger.info);
+        this._handleLastMessage(arguments, 'info');
     }
 
     warn() {
         arguments[0] = ' ' + arguments[0];
-        this.handleLastMessage(arguments, this.winstonLogger.warn);
+        this._handleLastMessage(arguments, 'warn');
     }
 
     error() {
-        this.handleLastMessage(arguments, this.winstonLogger.error);
+        this._handleLastMessage(arguments, 'error');
     }
 
-    debug() {
-        this.handleLastMessage(arguments, this.winstonLogger.debug);
-    }
-
-    logme(what, fnc) {
-        if (!cluster.forkNumber) return fnc(what);
+    logme(msg, level) {
+        if (!cluster.forkNumber) return console.log(this._prefix(level, msg));
 
         setTimeout(() => {
-            fnc(what);
+            console.log(this._prefix(level, msg));
         },cluster.forkNumber*4);
 
     }
 
-    handleLastMessage(args, fnc) {
+    _handleLastMessage(args, level) {
         this.message = util.format.apply(this, args);
 
         if (this.message != this.lastMessage) {
             this.lastMessage = this.message;
             this.lastMessageRepeat = 0;
-            return this.logme(this.message, fnc);
+            return this.logme(this.message, level);
         }
 
         this.lastMessageRepeat++;
 
         if (this.lastMessageRepeat > this.allowedMessageCount[this.allowedMessageCount.length]) {
-            return this.logme(' last message repeated more than '+ this.allowedMessageCount[this.allowedMessageCount.length]+' times: '+this.allowedMessageCount[this.allowedMessageCount.length], fnc);
+            return this.logme(' last message repeated more than '+ this.allowedMessageCount[this.allowedMessageCount.length]+' times: '+this.allowedMessageCount[this.allowedMessageCount.length], level);
         }
 
         if (this.allowedMessageCount.indexOf(this.lastMessageRepeat)>=0) {
-            return this.logme(' last message repeated '+this.lastMessageRepeat+' times: '+this.lastMessage, fnc);
+            return this.logme(' last message repeated '+this.lastMessageRepeat+' times: '+this.lastMessage, level);
         }
 
         if (this.lastMessageRepeat < 5) {
-            return this.logme(this.message, fnc);
+            return this.logme(this.message, level);
         }
     }
 
